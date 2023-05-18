e320219397c25ad99e39ea6dcadb911f871a13b5
AsadaSinon1 <103051656+AsadaSinon1@users.noreply.github.com>
2023-05-18 10:23:20 +0800

    third commit

--- src/code/Example.java
+++ src/code/Example.java
@@ -1,7 +1,7 @@
 package src.code;
 
-public class example{
+public class Example {
     public static void main(String[] args) {
-        System.out.println("SuperMario");
+        System.out.println("commit from IDEA");
     }
 }
