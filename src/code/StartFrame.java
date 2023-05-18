ab1215a773b0a33907e200cc7c0bfe3b2f35ff3d
AsadaSinon1 <103051656+AsadaSinon1@users.noreply.github.com>
2023-05-19 01:14:33 +0800

    rename-commit from Asada

--- src/code/StartFrame.java
+++ src/code/StartFrame.java
@@ -6,7 +6,7 @@ public class StartFrame extends JFrame {
     StartFrame(){
         setTitle("SuperMario");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        add(new TestMapBackground("src/image/bg.png").getBackground());
+        add(new TestMapBackground("src/image/UI参考.jpeg").getBackground());
         setSize(800,640);
         setResizable(false);
     }
