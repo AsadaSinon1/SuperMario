53ee0d8162ce26948933544c3f7a8b2104ec2864
FZYsheep <97429610+FZYsheep@users.noreply.github.com>
2023-05-28 01:07:14 +0800

    Update GameEnterPanel.java

--- src/code/GameEnterPanel.java
+++ src/code/GameEnterPanel.java
@@ -19,10 +19,10 @@ public class GameEnterPanel extends JPanel {
         this.add(background);
 
         button_start = new JButton("开始游戏");
-        button_start.setBounds(200,320,90,30);
+        button_start.setBounds(200,360,90,30);
 
         button_end = new JButton("退出游戏");
-        button_end.setBounds(200,360,90,30);
+        button_end.setBounds(200,420,90,30);
         button_end.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 System.exit(0);//退出程序
